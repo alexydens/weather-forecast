9a548f48577ccd49466d810f9251c5625416fcfe
alexydens <alexydens@outlook.com>
2023-08-14 16:57:08 +0100

    API call added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,80 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
-  return <div className="bg-slate-500">App</div>;
+  const [weather, setWeather] = useState({
+    // some dummy data from the api's documentation to introduce all the fields
+    coord: {
+      lon: 10.99,
+      lat: 44.34,
+    },
+    weather: [
+      {
+        id: 501,
+        main: "Rain",
+        description: "moderate rain",
+        icon: "10d",
+      },
+    ],
+    base: "stations",
+    main: {
+      temp: 298.48,
+      feels_like: 298.74,
+      temp_min: 297.56,
+      temp_max: 300.05,
+      pressure: 1015,
+      humidity: 64,
+      sea_level: 1015,
+      grnd_level: 933,
+    },
+    visibility: 10000,
+    wind: {
+      speed: 0.62,
+      deg: 349,
+      gust: 1.18,
+    },
+    rain: {
+      "1h": 3.16,
+    },
+    clouds: {
+      all: 100,
+    },
+    dt: 1661870592,
+    sys: {
+      type: 2,
+      id: 2075663,
+      country: "IT",
+      sunrise: 1661834187,
+      sunset: 1661882248,
+    },
+    timezone: 7200,
+    id: 3163858,
+    name: "Zocca",
+    cod: 200,
+  });
+  useEffect(() => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?lat=51.50853&lon=-0.12574&appid=f47c8daefb3cc0c6c91903e89f44433a"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setWeather(data);
+      });
+  }, []);
+  return (
+    <div className="bg-slate-500">
+      {weather.weather[0].main}
+      <img
+        src={
+          "https://openweathermap.org/img/wn/" +
+          weather.weather[0].icon +
+          "@2x.png"
+        }
+        alt="icon"
+      />
+    </div>
+  );
 };
 
 export default App;
